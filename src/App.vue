f2e55c26c597b8a0478eb80cd6215a685de11f39
expressobyte <tajulasri@codexpert.my>
2019-12-20 02:05:04 +0800

    add navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
     <v-app-bar app v-if="isLoggedIn">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title class="headline text-uppercase">
         <span>Vuetify</span>
         <span class="font-weight-light">MATERIAL DESIGN</span>
@@ -12,6 +13,36 @@
     </v-app-bar>
     <v-content>
       <router-view></router-view>
+      <v-navigation-drawer v-model="drawer" absolute temporary>
+        <v-list-item>
+          <v-list-item-avatar>
+            <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>John Leider</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list dense>
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+            @click="$router.push(item.route)"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
     </v-content>
   </v-app>
 </template>
@@ -19,7 +50,16 @@
 <script>
 export default {
   name: "App",
-  data: () => ({}),
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: "Home", icon: "dashboard", route: null },
+      { title: "Companies", icon: "dashboard", route: "companies" },
+      { title: "Staffs", icon: "dashboard", route: "staffs" },
+      { title: "Attractions", icon: "dashboard", route: null },
+      { title: "About", icon: "question_answer", route: null }
+    ]
+  }),
   watch: {
     isLoggedIn(val) {
       if (!val) {
